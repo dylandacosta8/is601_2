12b3681caac8a8fe7953cc04d7feb075b6e38994
Dylan Da Costa <dylan2222@gmail.com>
2024-09-15 16:56:30 -0400

    added test cases for multiplication and division functions

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,11 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(5,2) == 3
+
+def test_multiplication():
+    '''Test that addition function works '''    
+    assert multiply(8,8) == 64
+
+def test_division():
+    '''Test that addition function works '''    
+    assert divide(5,5) == 1.0
